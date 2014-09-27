0a9f12fd0aabb8c7053cf2672d70c02967224768
Julian Sparber <julian@sparber.net>
2014-09-27 03:38:40 +0200

    [feat] add request function

--- main.js
+++ main.js
@@ -321,3 +321,15 @@ function scale() {
     return ratio;
   }
 }
+APIrequest(:http://192.168.26.109/ajax/busstops.php");
+
+function APIrequest(url) {
+ function reqListener () {
+     console.log(this.responseText);
+ }
+
+ var oReq = new XMLHttpRequest();
+ oReq.onload = reqListener;
+ oReq.open("get", url, true);
+ oReq.send();
+}
